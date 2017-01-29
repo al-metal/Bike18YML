b9effe12eadb52308d510918ecf00008747fee20
al-metal <al-metal@bk.ru>
2017-01-29 20:32:05 +0400

    Получение разделов с гл. страницы
    Получение куки байк18

--- Bike18YML/Form1.cs
+++ Bike18YML/Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Bike18;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +15,8 @@ namespace Bike18YML
 {
     public partial class Form1 : Form
     {
+        nethouse nethouse = new nethouse();
+        httpRequest request = new httpRequest();
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +31,29 @@ namespace Bike18YML
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            #region pass/login
             Properties.Settings.Default.loginBike18 = tbLogin.Text;
             Properties.Settings.Default.passwordBike18= tbPassword.Text;
             Properties.Settings.Default.Save();
+            #endregion
+
+            string otv = "";
+
+            CookieContainer cookie = nethouse.CookieNethouse(tbLogin.Text, tbPassword.Text);
+
+            if(cookie.Count != 4)
+            {
+                MessageBox.Show("Введены не верные пароли!");
+                return;
+            }
+
+            otv = request.getRequest("https://bike18.ru/");
+            MatchCollection razdel = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*(?=\" class=\"blue\">)").Matches(otv);
+            for(int r = 0; razdel.Count > r; r++)
+            {
+                string urlRazdel = razdel[r].ToString();
+            }
+
         }
     }
 }
